d222c6490fce74ed5ceac170afb3a70814878e76
AlexisK616 <alexiskremis@gmail.com>
2026-05-01 16:45:21 -0300

    add eureka cliente

--- src/main/java/com/alexis/springcloud/mscv/product/entities/Product.java
+++ src/main/java/com/alexis/springcloud/mscv/product/entities/Product.java
@@ -1,11 +1,6 @@
 package com.alexis.springcloud.mscv.product.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
@@ -24,6 +19,9 @@ public class Product {
 
 	@Column(name = "create_at")
 	private LocalDate createAt;
+
+	@Transient
+	private Integer port;
 
 	public Long getId() {
 		return id;
@@ -55,5 +53,13 @@ public class Product {
 
 	public void setCreateAt(LocalDate createAt) {
 		this.createAt = createAt;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
 	}
 }
--- src/main/java/com/alexis/springcloud/mscv/product/services/impl/ProductServiceImpl.java
+++ src/main/java/com/alexis/springcloud/mscv/product/services/impl/ProductServiceImpl.java
@@ -5,10 +5,12 @@ import com.alexis.springcloud.mscv.product.entities.Product;
 import com.alexis.springcloud.mscv.product.repository.ProductRepository;
 import com.alexis.springcloud.mscv.product.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -16,14 +18,23 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    Environment environment;
+
     @Override
     public List<Product> findAll() {
-        return productRepository.findAll();
+      return  productRepository.findAll().stream().map(product -> {
+            product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+            return product;
+        }).collect(Collectors.toList());
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return productRepository.findById(id);
+        return productRepository.findById(id).map(product -> {
+            product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+            return product;
+        });
     }
 
     @Override
